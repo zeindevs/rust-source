2620ecd53bca5223fd23ced4a04a840a7e188c7f
zeindevs <zeindevs@gmail.com>
2024-10-26 09:32:00 +0700

    add: rust-cli-guess

---
+++ rust-cli-guess/src/main.rs
@@ -0,0 +1,37 @@
+extern crate rand;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("--- Geuss the number ---");
+
+    let secret_number = rand::thread_rng().gen_range(1..20);
+
+    loop {
+        println!("Please input your number:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        println!("Your guess: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Error - please input a number");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
